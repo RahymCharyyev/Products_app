7509df63ae54b8b8e43ab5f9d9ea0b3ae394a041
RahymCharyev <r569837@gmail.com>
2023-01-29 17:02:34 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Loader } from "./components/Loader";
+import { ErrorMessage } from "./components/ErrorMessage";
+import { Product } from "./components/Product";
+import { useProducts } from "./hooks/products";
+import { Modal } from "./components/Modal";
+import { CreateProduct } from "./components/CreateProduct";
 
 function App() {
+  const { loading, error, products } = useProducts();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto max-w-2xl pt-5">
+      {loading && <Loader />}
+      {error && <ErrorMessage error={error} />}
+      {products.map((product) => (
+        <Product product={product} key={product.id} />
+      ))}
+      <Modal title="Create new Product">
+        <CreateProduct />
+      </Modal>
     </div>
   );
 }
